570d41978699445826475656ae69342bd132a88e
Jennifer Mulroy <jennifer.mulroy@gmail.com>
2020-02-16 13:09:35 -0500

    added commentary to files

---
+++ Scripts/.ipynb_checkpoints/db_config-checkpoint.py
@@ -0,0 +1,5 @@
+PGHOST = 'project-3.cdr354qikgy9.us-east-2.rds.amazonaws.com'
+PGDATABASE = 'project-3'
+PGUSER = 'NBA'
+PGPASSWORD = 'NBAFun23!'
+
--- Scripts/db_config.py
+++ Scripts/db_config.py
@@ -1,4 +1,5 @@
 PGHOST = 'project-3.cdr354qikgy9.us-east-2.rds.amazonaws.com'
 PGDATABASE = 'project-3'
-PGUSER = 'jennifer.mulroy@gmail.com'
+PGUSER = 'NBA'
 PGPASSWORD = 'NBAFun23!'
+
